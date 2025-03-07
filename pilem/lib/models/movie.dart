90b38342ccfb0d9b5c3dfd930646a2a8a6ec98e3
Axel Theo Winata Ursia <ugraagung30@gmail.com>
2025-03-07 18:05:58 +0700

    project lanjutan film

---
+++ pilem/lib/models/movie.dart
@@ -0,0 +1,45 @@
+// lib/models/movie.dart
+
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterPath;
+  final String backdropPath;
+  final String releaseDate;
+  final double voteAverage;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+    required this.backdropPath,
+    required this.releaseDate,
+    required this.voteAverage,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      overview: json['overview'] as String,
+      posterPath: json['poster_path'] as String,
+      backdropPath: json['backdrop_path'] as String,
+      releaseDate: json['release_date'] as String,
+      voteAverage: (json['vote_average'] as num).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'overview': overview,
+      'poster_path': posterPath,
+      'backdrop_path': backdropPath,
+      'release_date': releaseDate,
+      'vote_average': voteAverage,
+    };
+  }
+}
